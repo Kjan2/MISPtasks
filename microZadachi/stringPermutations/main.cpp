8d1e3739ecf9cf2dd0a0f9efd987d055313edf35
kjan2 <kjan@test.com>
2022-10-13 18:52:53 +0300

    rofl

--- microZadachi/stringPermutations/main.cpp
+++ microZadachi/stringPermutations/main.cpp
@@ -1,23 +1,13 @@
 #include <algorithm>
 #include <string>
 #include <iostream>
-
-void printPermutations(std::string s)
-{
-    std::sort(s.begin(), s.end());
-    do {
-        std::cout << s << std::endl;
- std   } while (std::next_permutation(s.begin(), s.end()));
-}
-x
-void perm(std::string s)
-{
-
-}
-
+ 
 int main()
 {
     std::string s;
     std::cin >> s;
-    printPermutations(s);
+    std::sort(s.begin(), s.end());
+    do {
+        std::cout << s << std::endl;
+    } while(std::next_permutation(s.begin(), s.end()));
 }
